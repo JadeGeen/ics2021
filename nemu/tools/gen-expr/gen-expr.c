f3cfd57005b8e2e4a5da6c7ae74e85701c1de6f9
tracer-ics2021 <tracer@njuics.org>
2021-09-27 19:46:32 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 820a115d964c48db9fd4291f4f8f8c6e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  19:46:32 up  3:45,  3 users,  load average: 0.38, 0.33, 0.39

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,39 +16,109 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static int index=0;
+
+#include<time.h>
+
+static uint32_t choose(uint32_t n){
+	randomize();
+	uint32_t x = rand()%n;
+	return x;
+}
+
+
+static void gen(int x){
+	if(index>=65536){
+		return;
+	}
+	switch(x){
+		case '(':
+			buf[index]="\\(";
+			index++;
+			break;
+		case ')':
+			buf[index]="\\)";
+			index++;
+			break;
+		default:break;
+	}
+}
+
+static void gen_num(){
+	uint32_t newnum=choose(65536);
+	char newnumber[4];
+	ultoa(newnum,newnumer,10);
+	int length = strlen(newnumber);
+	for (int i = 0;i<length;i++){
+		if(index>=65536){
+			break;
+		}
+		buf[index]=newnumber[i];
+		index++;
+}
+
+static void gen_rand_op(){
+	if(index>=65536){
+		return;
+	}
+	switch (choose(4)){
+		case 0:
+			buf[index]="\\+";
+			index++;
+			break;
+		case 1:
+			buf[index]="-";
+			index++;
+			break;
+		case 2:
+			buf[index]="\\*";
+			index++;
+			break;
+		default:
+			buf[index]="/";
+			index++;
+			break;
+	}
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+	buf[0] = '\0';
+	switch (choose(3)) {
+		case 0: gen_num(); break;
+		case 1: gen('('); gen_rand_expr(); gen(')'); break;
+		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;								   
+	}
 }
 
 int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+	int seed = time(0);
+	srand(seed);
+	int loop = 1;
+	if (argc > 1) {
+		sscanf(argv[1], "%d", &loop);
+	}
+	int i;
+	for (i = 0; i < loop; i ++) {
+		gen_rand_expr();
 
-    sprintf(code_buf, code_format, buf);
+		sprintf(code_buf, code_format, buf);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+		FILE *fp = fopen("/tmp/.code.c", "w");
+		assert(fp != NULL);
+		fputs(code_buf, fp);
+		fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+		int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+		if (ret != 0) continue;
 
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
+		fp = popen("/tmp/.expr", "r");
+		assert(fp != NULL);
 
-    int result;
-    fscanf(fp, "%d", &result);
-    pclose(fp);
+		int result;
+		fscanf(fp, "%d", &result);
+		pclose(fp);
 
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+		printf("%u %s\n", result, buf);
+	}
+	return 0;
 }
