71b1612475f819e4c4aa08760f4252e63c049a49
tracer-ics2021 <tracer@njuics.org>
2021-09-29 21:21:13 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 24e16fbd104a43d0bd110a3c7306ae72   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:21:13 up  1:59,  3 users,  load average: 0.14, 0.21, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -171,7 +171,7 @@ word_t expr(char *e, bool *success) {
 
 		int i;
 		for(i=0;i<nr_token;i++){
-			if(tokens[i].type=='-' &&(i == 0 || tokens[i - 1].type == '+' || tokens[i - 1].type == '-' || tokens[i - 1].type == '*' || tokens[i - 1].type == '/')){
+			if(tokens[i].type=='-' &&(i == 0 || (tokens[i - 1].type != TK_NUMBER && tokens[i - 1].type != TK_HEX && tokens[i - 1].type != TK_REG) )){
 				tokens[i].type=TK_NEG;
 				tokens[i].priority=6;
 			}
