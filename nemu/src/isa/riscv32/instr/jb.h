e1726c692b1a514d484cd7e8e92d1f89f254a36c
tracer-ics2021 <tracer@njuics.org>
2021-10-30 18:04:23 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: e61e01700e484c579d179e2dee03a679   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:04:23 up  7:05,  4 users,  load average: 0.21, 0.26, 0.51

--- nemu/src/isa/riscv32/instr/jb.h
+++ nemu/src/isa/riscv32/instr/jb.h
@@ -1,4 +1,9 @@
 def_EHelper(jal){
-	s->pc = s->pc + id_src2->simm;
+	s->pc = s->pc + id_src2->imm;
+	rtl_li(s, ddest, 4 + s->pc);
+}
+
+def_EHelper(jalr){
+	s->pc = *dsrc1 + id_src2->imm;
 	rtl_li(s, ddest, 4 + s->pc);
 }
