223bfe33648980cf516fca3fc3df93eff49d1580
tracer-ics2021 <tracer@njuics.org>
2021-10-30 22:59:12 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: e61e01700e484c579d179e2dee03a679   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:59:12 up 12:00,  2 users,  load average: 0.44, 0.22, 0.16

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -4,24 +4,24 @@
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
-	0x00000413,          	//li	s0,0
-	0x00009117,         	//auipc	sp,0x9
-	0xffc10113,
-	0x00c000ef ,         	//jal	ra,80000018 <_trm_init>
-	0x00000513  ,        	//li	a0,0
-	0x00008067  ,		//        	ret
-	0x80000537   ,       	//lui	a0,0x80000
-	0xff010113    ,      	//	addi	sp,sp,-16
-	0x03850513     ,     	//	addi	a0,a0,56 # 80000038 <_end+0xffff7038>
-	0x00112623      ,    	//	sw	ra,12(sp)
-	0xfe9ff0ef       ,   	//	jal	ra,80000010 <main>
-	0x00050513        ,  	//	mv	a0,a0
-	0x0000006b         , 	//	0x6b
-	0x0000006f          ,	//	j	80000034 <_trm_init+0x1c>
-  //0x800002b7,  // lui t0,0x80000
-  //0x0002a023,  // sw  zero,0(t0)
-  //0x0002a503,  // lw  a0,0(t0)
-  //0x0000006b,  // nemu_trap
+	//0x00000413,          	//li	s0,0
+//	0x00009117,         	//auipc	sp,0x9
+//	0xffc10113,
+//	0x00c000ef ,         	//jal	ra,80000018 <_trm_init>
+//	0x00000513  ,        	//li	a0,0
+//	0x00008067  ,		//        	ret
+//	0x80000537   ,       	//lui	a0,0x80000
+//	0xff010113    ,      	//	addi	sp,sp,-16
+//	0x03850513     ,     	//	addi	a0,a0,56 # 80000038 <_end+0xffff7038>
+//	0x00112623      ,    	//	sw	ra,12(sp)
+//	0xfe9ff0ef       ,   	//	jal	ra,80000010 <main>
+//	0x00050513        ,  	//	mv	a0,a0
+//	0x0000006b         , 	//	0x6b
+//	0x0000006f          ,	//	j	80000034 <_trm_init+0x1c>
+  0x800002b7,  // lui t0,0x80000
+  0x0002a023,  // sw  zero,0(t0)
+  0x0002a503,  // lw  a0,0(t0)
+  0x0000006b,  // nemu_trap
 };
 
 static void restart() {
