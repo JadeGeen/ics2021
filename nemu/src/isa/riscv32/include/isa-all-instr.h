0d74c8c941f12becd006369be6b3d2702ac45907
tracer-ics2021 <tracer@njuics.org>
2021-10-30 19:19:57 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: e61e01700e484c579d179e2dee03a679   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  19:19:57 up  8:20,  4 users,  load average: 0.71, 0.70, 0.54

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(auipc) f(addi) f(jal)
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(auipc) f(addi) f(jal) f(jalr)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv32/instr/jb.h
+++ nemu/src/isa/riscv32/instr/jb.h
@@ -1,9 +1,9 @@
 def_EHelper(jal){
-	s->pc = s->pc + id_src2->imm;
+	s->dnpc = s->pc + id_src1->imm;
 	rtl_li(s, ddest, 4 + s->pc);
 }
 
 def_EHelper(jalr){
-	s->pc = *dsrc1 + id_src2->imm;
+	s->dnpc = *dsrc1 + id_src2->imm;
 	rtl_li(s, ddest, 4 + s->pc);
 }
