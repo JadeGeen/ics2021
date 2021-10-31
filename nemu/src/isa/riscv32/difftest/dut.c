3eeae9278ce563ea2cccf6503f22af153f63d61e
tracer-ics2021 <tracer@njuics.org>
2021-10-31 22:10:25 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 16cf2ceec64942fe8deb8fff27bee184   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:10:25 up 11:43,  1 user,  load average: 1.37, 1.00, 0.74

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -1,9 +1,14 @@
 #include <isa.h>
 #include <cpu/difftest.h>
-#include "../local-include/reg.h"
+
+extern const char* regs[];
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+	for(int i=0;i<32;i++){
+		if(!difftest_check_reg(regs[i],pc,ref_r->gpr[i]._32,cpu.gpr[i]._32))
+				return false;
+	}
+   	return true;
 }
 
 void isa_difftest_attach() {
