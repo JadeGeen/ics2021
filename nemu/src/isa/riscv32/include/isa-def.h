4ee554c21b1716e236ec2f94aeec0c575a1eef00
tracer-ics2021 <tracer@njuics.org>
2021-11-01 10:01:01 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 851a8fe0573f4ef697cf77547a4cf76e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  10:01:01 up  1:36,  3 users,  load average: 0.19, 0.20, 0.15

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -46,6 +46,26 @@ typedef struct {
 			uint32_t simm10_1  :10;
 			int32_t  simm20    : 1;
 		} j;
+		struct{
+			uint32_t opcode1_0 : 2;
+			uint32_t opcode6_2 : 5;
+			uint32_t rd        : 5;
+			uint32_t funct3    : 3;
+			uint32_t rs1       : 5;
+			uint32_t rs2       : 5;
+			uint32_t funct7    : 7;
+		} r;
+		struct{
+			uint32_t opcode1_0 : 2;
+			uint32_t opcode6_2 : 5;
+			uint32_t simm11    : 1;
+			uint32_t simm4_1   : 4;
+			uint32_t funct3    : 3;
+			uint32_t rs1       : 5;
+			uint32_t rs2       : 5;
+			uint32_t simm10_5  : 5;
+			int32_t  simm12    : 1; 
+		} b;
 		uint32_t val;
 	} instr;
 } riscv32_ISADecodeInfo;
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -9,3 +9,11 @@ def_EHelper(addi){
 def_EHelper(auipc){
 	rtl_li(s, ddest, id_src1->imm + s->pc);	
 }
+
+def_EHelper(add){
+	rtl_add(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sub){
+	rtl_sub(s, ddest, dsrc1, dsrc2);
+}
