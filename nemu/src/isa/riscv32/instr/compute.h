564ee64b43e1c7c630a57f7260dc041c05940e07
tracer-ics2021 <tracer@njuics.org>
2021-11-01 10:31:58 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 851a8fe0573f4ef697cf77547a4cf76e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  10:31:58 up  2:07,  3 users,  load average: 0.45, 0.34, 0.28

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -2,12 +2,21 @@ def_EHelper(lui) {
 	rtl_li(s, ddest, id_src1->imm);
 }
 
+def_EHelper(auipc){
+	rtl_li(s, ddest, id_src1->imm + s->pc);	
+}
+
 def_EHelper(addi){
 	rtl_addi(s, ddest, dsrc1, id_src2->imm);
 }
 
-def_EHelper(auipc){
-	rtl_li(s, ddest, id_src1->imm + s->pc);	
+def_EHelper(sltiu){
+	int32_t temp = *dsrc1;
+	int32_t temp2 = id_src2->imm;
+	if(temp < temp2)
+	rtl_li(s, ddest, 1);
+	else
+	rtl_li(s, ddest, 0);
 }
 
 def_EHelper(add){
