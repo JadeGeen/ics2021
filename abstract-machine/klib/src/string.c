d1229acaafdd5c9f7a39d1b17cdadfa341c01698
tracer-ics2021 <tracer@njuics.org>
2021-11-02 21:38:30 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 71df0638e30a4832a383e93470d2bf94   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:38:30 up  3:26,  3 users,  load average: 0.64, 0.49, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,169 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
+size_t strlen(const char *s)
+{
+	if (s == NULL)
+		return 0;
+	size_t length = 0;
+	for(length=0;s[length]!='\0';length++)
+	;
+	
+	return length;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *dst, const char *src)
+{
+	if(dst==NULL)
+		return NULL;
+	if(src==NULL)
+		return dst;
+	size_t i;
+	for(i=0;src[i]!='\0';i++)
+		dst[i]=src[i];
+	dst[i]='\0';
+	return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strncpy(char *dst, const char *src, size_t n)
+{
+	if(dst==NULL)
+		return NULL;
+	if(src==NULL)
+		return dst;
+	size_t i;
+	for(i=0;i<n&&src[i]!='\0';i++)
+		dst[i]=src[i];
+	for(;i<=n;i++)
+		dst[i]='\0';
+	return dst;
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcat(char *dst, const char *src)
+{
+	if (dst == NULL)
+		return NULL;
+	if (src == NULL)
+		return dst;
+	size_t dst_len=strlen(dst);
+	size_t i;
+	for(i=0;src[i]!='\0';i++)
+		dst[dst_len+i]=src[i];
+	dst[dst_len+i]='\0';
+	return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+int strcmp(const char *s1, const char *s2)
+{
+	if(s1==NULL&&s2==NULL)
+		return 0;
+	if(s1!=NULL&&s2==NULL)
+		return 1;
+	if(s1==NULL&&s2!=NULL)
+		return -1;
+	size_t i;
+	for(i=0;s1[i]!='\0'&&s2[i]!='\0';i++){
+		if(s1[i]==s2[i])
+			continue;
+		else if(s1[i]>s2[i])
+			return 1;
+		else
+			return -1;
+	}
+	if(s1[i]=='\0'&&s2[i]=='\0')
+		return 0;
+	else if(s1[i]!='\0'&&s2[i]=='\0')
+		return 1;
+	else 
+		return -1;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+	if(s1==NULL&&s2==NULL)
+		return 0;
+	if(s1!=NULL&&s2==NULL)
+		return 1;
+	if(s1==NULL&&s2!=NULL)
+		return -1;
+	size_t i;
+	for(i=0;s1[i]!='\0'&&s2[i]!='\0'&&i<n;i++){
+		if(s1[i]==s2[i])
+			continue;
+		else if(s1[i]>s2[i])
+			return 1;
+		else
+			return -1;
+	}
+	if(i==n&&s1[i]==s2[i])
+		return 0;
+	if(s1[i]=='\0'&&s2[i]=='\0')
+		return 0;
+	else if(s1[i]!='\0'&&s2[i]=='\0')
+		return 1;
+	else 
+		return -1;
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *s, int c, size_t n)
+{
+	if(s==NULL||n<=0)
+		return 0;
+	const unsigned char u=c;
+	unsigned char *us;
+	for(us=s;n>0;us++,n--)
+		*us=u;
+	return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n)
+{
+	if(dst==NULL||src==NULL||n<0)
+		return NULL;
+	unsigned char *udst=(unsigned char*)dst;
+	unsigned char *usrc=(unsigned char*)src;
+	size_t i;
+	if(dst<src)
+		for(i=0;i<n;i++)
+			*(udst+i)=*(usrc+i);
+	else
+		for(i=n-1;i>=0;i--)
+			*(udst+i)=*(usrc+i);
+	return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+void *memcpy(void *out, const void *in, size_t n)
+{
+	if(out==NULL||in==NULL||n<0)
+		return NULL;
+	unsigned char *uout=(unsigned char*)out;
+	unsigned char *uin=(unsigned char*)in;
+	size_t i;
+	for(i=0;i<n;i++)
+		*(uout+i)=*(uin+i);
+	return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	if(s1==NULL&&s2==NULL)
+		return 0;
+	if(s1!=NULL&&s2==NULL)
+		return 1;
+	if(s1==NULL&&s2!=NULL)
+		return -1;
+	size_t i;
+	unsigned char *us1=(unsigned char*)s1;
+	unsigned char *us2=(unsigned char*)s2;
+	for(i=0;i<n;i++){
+		if(*(us1+i)==*(us2+i))
+			continue;
+		else if(*(us1+i)>*(us2+i))
+			return 1;
+		else
+			return -1;
+	}
+	return 0;
 }
 
 #endif
