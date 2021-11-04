9351dd15b8de372fed739a27ecef89750783cc68
tracer-ics2021 <tracer@njuics.org>
2021-11-04 22:12:42 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 614c22e8653349da8e1f485171eb7b9e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  22:12:42 up  1:51,  2 users,  load average: 0.51, 0.39, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,64 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char *itoa(int value, char *string, int radix){
+	int i;
+	char temp[100]={0};
+	for(i=0;value>0;i++){
+		temp[i]=value%radix;
+		value=value/radix;
+	}
+	for(int j=i-1;j>=0;j++){
+		*string=temp[j];
+		string++;
+	}
+	return string;//directly return last point
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	char *str;
+	char *s;
+	int len;
+	for(str=out;*fmt;fmt++){
+		fmt++;//skip the first'%'
+		switch(*fmt){
+			case 's':
+				s=va_arg(ap, char*);
+				len=strlen(s);
+				for(int i=0;i<len;i++){
+					*str=*s;
+					str++;
+					s++;
+				}
+				break;
+			case 'd':
+				str=itoa(va_arg(ap, int),str,10);
+		}
+	}
+	*str='\0';
+	int ret=str-out;
+	return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list args;
+	int i;
+	va_start(args, fmt);
+	i=vsprintf(out, fmt, args);
+	va_end(args);
+	return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 #endif
