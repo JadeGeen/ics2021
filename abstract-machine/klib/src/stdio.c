dcacb6a9250554e3e4f467fd22cc071440933a3d
tracer-ics2021 <tracer@njuics.org>
2021-11-27 12:00:33 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 4c439c52f8464dad8615369dd0963891   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:00:33 up  1:18,  1 user,  load average: 0.30, 0.31, 0.34

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -302,7 +302,7 @@ int sprintf(char *out, const char *fmt, ...) {
 int printf(const char *fmt, ...){
 	va_list args;
 	int i;
-	char buf[2048]={'\0'};
+	char buf[16384];
 	va_start(args,fmt);
 	i=vsprintf(buf, fmt, args);
 	va_end(args);
