afb1e1d0b927999a861bd969bc2279de5afbd241
tracer-ics2021 <tracer@njuics.org>
2021-11-27 20:52:31 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 4c439c52f8464dad8615369dd0963891   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:52:31 up 10:10,  1 user,  load average: 0.40, 0.39, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,6 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  		kbd->keydown = ((inl(KBD_ADDR))&0x8000)>>15;
-  		kbd->keycode = (inl(KBD_ADDR))&0xffff7fff;
+  		kbd->keydown = ((inl(KBD_ADDR))&0x8000)?true:false;
+  		kbd->keycode = (inl(KBD_ADDR))&0xff;
 }
