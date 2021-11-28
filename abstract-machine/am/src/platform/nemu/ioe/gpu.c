e19783640cc2a46161f7de0e797d9161ec4b8abf
tracer-ics2021 <tracer@njuics.org>
2021-11-28 12:51:38 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 0a71e721a87644b2bcd500cf25170c36   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:51:38 up  2:38,  1 user,  load average: 0.32, 0.30, 0.22

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,9 +23,8 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  	if (w == 0 || h == 0) return;
   	uint32_t width=inw(VGACTL_ADDR+2);
-  	uint32_t *pixels=ctl->pixels;
+  	uint32_t *pixels=(uint32_t *)ctl->pixels;
   	int i=0;
   	for(int y0=y;y0<y+h;y0++)
   		for(int x0=x;x0<x+w;x0++,i++)
