b1251075918c8c126b4cb8dfb9363f6a91f41eb3
tracer-ics2021 <tracer@njuics.org>
2021-11-28 12:25:22 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 0a71e721a87644b2bcd500cf25170c36   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:25:22 up  2:11,  1 user,  load average: 0.40, 0.48, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+/*
 	int i;
 	uint32_t w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
 	uint32_t h = inw(VGACTL_ADDR);  // TODO: get the correct height
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 	for (i = 0; i < w * h; i ++) fb[i] = i;
 	outl(SYNC_ADDR, 1);
+	*/
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
