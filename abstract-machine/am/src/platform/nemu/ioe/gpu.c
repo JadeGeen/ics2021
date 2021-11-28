466fe3c5dcc6a17048fa9be3670820ec55591ab0
tracer-ics2021 <tracer@njuics.org>
2021-11-28 12:21:36 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 0a71e721a87644b2bcd500cf25170c36   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:21:36 up  2:08,  1 user,  load average: 0.11, 0.16, 0.13

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
 	int i;
-	int w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
-	int h = inw(VGACTL_ADDR);  // TODO: get the correct height
+	uint32_t w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
+	uint32_t h = inw(VGACTL_ADDR);  // TODO: get the correct height
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 	for (i = 0; i < w * h; i ++) fb[i] = i;
 	outl(SYNC_ADDR, 1);
@@ -23,12 +23,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   	if (w == 0 || h == 0) return;
-  	int width=(VGACTL_ADDR+2);
+  	uint32_t width=inw(VGACTL_ADDR+2);
   	uint32_t *pixels=(uint32_t *)ctl->pixels;
   	int i=0;
   	for(int y0=y;y0<y+h;y0++)
   		for(int x0=x;x0<x+w;x0++,i++)
-  			outl(FB_ADDR+y0*width+x0,pixels[i]);
+  			outl(FB_ADDR+4*y0*width+4*x0,pixels[i]);
   	if (ctl->sync) {
     		outl(SYNC_ADDR, 1);
   	}
