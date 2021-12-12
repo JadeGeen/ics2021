16cb7f58dc99126dfd8df216a17fb79d3ed99cff
tracer-ics2021 <tracer@njuics.org>
2021-12-12 19:10:08 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 49477d218658425aa740017c505611b1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:10:08 up 10:19,  3 users,  load average: 0.55, 0.59, 0.62

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(auipc) f(addi) f(jal) f(jalr) f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and) f(mul) f(mulh) f(mulhu) f(div) f(divu) f(rem) f(remu) f(slti) f(sltiu) f(ori) f(xori) f(andi) f(slli) f(srli) f(srai) f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(sb) f(sh) f(lb) f(lh) f(lbu) f(lhu) f(csrrs) f(mret) f(ecall)
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(auipc) f(addi) f(jal) f(jalr) f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and) f(mul) f(mulh) f(mulhu) f(div) f(divu) f(rem) f(remu) f(slti) f(sltiu) f(ori) f(xori) f(andi) f(slli) f(srli) f(srai) f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(sb) f(sh) f(lb) f(lh) f(lbu) f(lhu) f(csrrw) f(csrrs) f(mret) f(ecall)
 
 def_all_EXEC_ID();
