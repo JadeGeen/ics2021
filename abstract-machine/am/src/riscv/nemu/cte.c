f7fdf82444b89e2251bf340059ed0c84f0057487
tracer-ics2021 <tracer@njuics.org>
2022-01-08 10:12:40 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 8d5398d796b649b9bb30c3bc67b9e8da   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  10:12:40 up 32 min,  3 users,  load average: 0.08, 0.11, 0.09

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,41 +5,42 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  if (user_handler) {
-    Event ev = {0};
-    switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
-    }
+	if (user_handler) {
+		Event ev = {0};
+		switch (c->mcause) {
+			case 1: ev.event = EVENT_YIELD; break;
+			default: ev.event = EVENT_ERROR; break;
+		}
 
-    c = user_handler(ev, c);
-    assert(c != NULL);
-  }
+		c = user_handler(ev, c);
+		assert(c != NULL);
+	}
 
-  return c;
+	return c;
 }
 
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+	// initialize exception entry
+	asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
-  // register event handler
-  user_handler = handler;
+	// register event handler
+	user_handler = handler;
 
-  return true;
+	return true;
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+	return NULL;
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+	asm volatile("li a7, -1; ecall");
 }
 
 bool ienabled() {
-  return false;
+	return false;
 }
 
 void iset(bool enable) {
