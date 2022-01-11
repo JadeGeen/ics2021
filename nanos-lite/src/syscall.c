98996cadcd825802e832eb916fd02bcc60bbf044
tracer-ics2021 <tracer@njuics.org>
2022-01-11 21:07:24 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 2938cbbbb1fb49579e78ebd45a71fc06   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:07:24 up 1 day,  2:07,  2 users,  load average: 0.25, 0.39, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,10 +5,10 @@ void do_syscall(Context *c) {
 	a[0] = c->GPR1;
 	Log("syscall %u",a[0]);
 	switch (a[0]) {
-		case 0://exit
+		/*case 0://exit
 			halt(0);
 			c->GPRx = 0;
-			break;
+			break;*/
 		case 1://yield
 			yield();
 			c->GPRx = 0;
