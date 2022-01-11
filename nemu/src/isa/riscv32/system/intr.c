3df6f0d19e172ce8d1541daccb510ef0ee0d8bc9
tracer-ics2021 <tracer@njuics.org>
2022-01-11 18:01:59 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 2938cbbbb1fb49579e78ebd45a71fc06   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  18:01:59 up 23:02,  1 user,  load average: 0.44, 0.35, 0.35

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,12 +4,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	/* TODO: Trigger an interrupt/exception with ``NO''.
 	 * Then return the address of the interrupt/exception vector.
 	 */
-	if(NO==1){
-		cpu.mepc = epc;
-		cpu.mcause = NO;
-		return cpu.mtvec;
-	}
-	return 0;
+	cpu.mepc = epc;
+	cpu.mcause = NO;
+	return cpu.mtvec;
+
 }
 
 word_t isa_query_intr() {
