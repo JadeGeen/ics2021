13cbf999fd4876d85da41f2d4b49f83c7947d6cc
tracer-ics2021 <tracer@njuics.org>
2022-01-12 16:23:34 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 1aba96caa9454f44a52d4c198376fe4f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  16:23:34 up  6:06,  1 user,  load average: 0.27, 0.36, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,15 +17,15 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	ramdisk_read(&elfhdr, 0, sizeof(Elf_Ehdr));
 	Elf_Phdr prohdr[elfhdr.e_phentsize];
 	size_t phentsize = elfhdr.e_phentsize;
-	//printf("phentsize=%d\n",phentsize);
-	//printf("entry=0x%08x\n",elfhdr.e_entry);
+	printf("phentsize=%d\n",phentsize);
+	printf("entry=0x%08x\n",elfhdr.e_entry);
 	for(int i = 0;i<elfhdr.e_phnum;i++)
 	{
 		ramdisk_read(&prohdr[i], elfhdr.e_phoff + i*phentsize, phentsize);
 		if(prohdr[i].p_type == PT_LOAD)
 		{
 			ramdisk_read((void*)prohdr[i].p_vaddr,prohdr[i].p_offset, prohdr[i].p_filesz);
-			//printf("vaddr = 0x%08x,offset = 0x%08x,filesz = 0x%08x\n",prohdr[i].p_vaddr,prohdr[i].p_offset, prohdr[i].p_filesz);
+			printf("vaddr = 0x%08x,offset = 0x%08x,filesz = 0x%08x\n",prohdr[i].p_vaddr,prohdr[i].p_offset, prohdr[i].p_filesz);
 			memset((void*)(prohdr[i].p_vaddr + prohdr[i].p_filesz), 0, prohdr[i].p_memsz - prohdr[i].p_filesz);
 		}
 	}
