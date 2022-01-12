71705e4c957bd22fd444f98248c4ec340d090225
tracer-ics2021 <tracer@njuics.org>
2022-01-12 20:21:29 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 1aba96caa9454f44a52d4c198376fe4f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  20:21:29 up 10:04,  1 user,  load average: 0.43, 0.47, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,7 +19,7 @@ void do_syscall(Context *c) {
 	a[1] = c->GPR2;
 	a[2] = c->GPR3;
 	a[3] = c->GPR4;
-	printf("syscall ID = %d, arg1 = %d, arg2 = %d, arg3 = %d\n", a[0], a[1], a[2], a[3]);
+	Log("syscall ID = %d, arg1 = %d, arg2 = %d, arg3 = %d\n", a[0], a[1], a[2], a[3]);
 	switch (a[0]) {
 		case SYS_exit://exit
 			halt(0);
