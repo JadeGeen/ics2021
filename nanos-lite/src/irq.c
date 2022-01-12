4ef3c5b038439fa57455c7d142c97d3a46f3deb1
tracer-ics2021 <tracer@njuics.org>
2022-01-12 18:28:38 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 1aba96caa9454f44a52d4c198376fe4f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  18:28:38 up  8:11,  1 user,  load average: 0.44, 0.43, 0.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
 	switch (e.event) {
 		case EVENT_YIELD: Log("yiled OK");break;
-		case EVENT_SYSCALL: printf("do_event %d\n",c->GPR1);do_syscall(c);break;
+		case EVENT_SYSCALL:do_syscall(c);break;
 		default: panic("Unhandled event ID = %d", e.event);
 	}
 
