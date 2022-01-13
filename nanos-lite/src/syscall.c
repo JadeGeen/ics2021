f05670300709c3273094b9fa802831b711304274
tracer-ics2021 <tracer@njuics.org>
2022-01-13 23:43:03 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: d6549dd9aeef4e818a200fcb7c509495   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  23:43:03 up  7:14,  1 user,  load average: 0.27, 0.28, 0.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -43,7 +43,7 @@ void do_syscall(Context *c) {
 			c->GPRx = fs_read(a[1], (void*)a[2], a[3]);
 			break;
 		case SYS_write:
-			c->GPRx = sys_write(a[1], (char*)a[2], a[3]);
+			c->GPRx = fs_write(a[1], (void*)a[2], a[3]);
 			break;
 		case SYS_close:
 			c->GPRx = fs_close(a[1]);
