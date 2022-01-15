174b8cf73ee953e28ec31ce845cced6228fe9a22
tracer-ics2021 <tracer@njuics.org>
2022-01-15 19:31:38 +0800

    >  compile 201300023    Static hostname: SharkWitch          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b8d340c469943c480cf1bdb5a522eda            Boot ID: 7b487a0feac945558c500c83c4aca108   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:31:38 up  2:32,  1 user,  load average: 0.66, 0.39, 0.31

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -2,14 +2,14 @@
 #include <NDL.h>
 
 int main(){
-	float t = -1;
-	float temp = -1;
+	int t = -1;
+	int temp = -1;
 	printf("time-test starts\n");
 	while(1)
 	{
 		t = NDL_GetTicks();
 		if(t / 2000 != temp){
-			temp = t/1000;
+			temp = t/2000;
 			printf("sec : %f\n",temp);
 		}
 	}
